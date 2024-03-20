cc302a9674e02abb7eeca2044335dbdcc8dca173
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-03-20 09:18:51 +0100

    CHUKS

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
+// const axios = require('axios');
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/direct/',
+//   params: {
+//     destination: 'LED',
+//     origin: 'MOW'
+//   },
+//   headers: {
+//     'X-Access-Token': '<REQUIRED>',
+//     'X-RapidAPI-Key': '725fb5b942msh802001ec5ca8863p134943jsnec754daad71f',
+//     'X-RapidAPI-Host': 'travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com'
+//   }
+// };
+
+// try {
+// 	const response =  axios.request(options);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+
 const axios = require('axios');
 
 const options = {
   method: 'GET',
-  url: 'https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/direct/',
-  params: {
-    destination: 'LED',
-    origin: 'MOW'
-  },
+  url: 'https://timetable-lookup.p.rapidapi.com/TimeTable/BOS/LAX/20191217/',
   headers: {
-    'X-Access-Token': '<REQUIRED>',
     'X-RapidAPI-Key': '725fb5b942msh802001ec5ca8863p134943jsnec754daad71f',
-    'X-RapidAPI-Host': 'travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com'
+    'X-RapidAPI-Host': 'timetable-lookup.p.rapidapi.com'
   }
 };
 
-try {
-	const response =  axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
+async function fetchData() {
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+fetchData();
